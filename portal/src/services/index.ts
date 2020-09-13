fa9c9784c8802d8f459c43a4e66f93785d15046c
vikrammodh0111 <vikrammodh0111@gmail.com>
2020-09-13 19:41:25 +0530

    removed portal from backend

--- portal/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AuthService } from './auth.service';
-export { default as LoggerService } from './logger.service';
-export { default as NetworkService } from './network.service';
